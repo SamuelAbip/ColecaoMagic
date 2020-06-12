a25d4219a64d4bdbd71bde4c63d41da46e64f596
Samuel Abip Gonçalves <samuelabip@gmail.com>
2020-06-12 01:56:07 -0300

    Construindo a opção de adicionar à lista, formatando a entrada, e imprimi-la.

---
+++ ColeçãoMagic/OpçõesMenu.cs
@@ -0,0 +1,10 @@
+﻿namespace ColeçãoMagic
+{
+    public enum OpçõesMenu
+    {
+        Sair = 0,
+        AdicionarCartas = 1,
+        ImprimirColeção = 2,
+        Inválida = 99
+    }
+}
--- ColeçãoMagic/Program.cs
+++ ColeçãoMagic/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Threading;
+using System.Globalization;
 
 namespace ColeçãoMagic
 {
     class Program
     {
+        private static List<string> Opções = new List<string>
+        {
+            "0 - Sair do programa.",
+            "1 - Adicionar cartas à sua coleção.",
+            "2 - Imprimir a sua coleção."
+        };
         private static List<string> Cartas = new List<string>();
         static void Main(string[] args)
         {
             Console.WriteLine("Bem-vindo ao collection builder de Magic. Escolha uma das opções:");
             while (true)
             {
+                ImprmirOpções();
                 OpçõesMenu opção = LerOpção();
                 RealizarAção(opção);
                 if (opção == OpçõesMenu.Sair)
@@ -23,9 +33,6 @@ namespace ColeçãoMagic
 
         private static OpçõesMenu LerOpção()
         {
-            Console.WriteLine("0 - Sair do programa.");
-            Console.WriteLine("1 - Adicionar cartas à sua coleção.");
-            Console.WriteLine("2 - Imprimir a sua coleção.");
             string opçãoEscolhidaString = Console.ReadLine();
             bool opçãoÉVálida = Enum.TryParse(opçãoEscolhidaString, out OpçõesMenu opçãoEscolhida);
             if (!opçãoÉVálida)
@@ -33,6 +40,14 @@ namespace ColeçãoMagic
                 return OpçõesMenu.Inválida;
             }
             else return opçãoEscolhida;
+        }
+
+        private static void ImprmirOpções()
+        {
+            foreach (var opção in Opções)
+            {
+                Console.WriteLine(opção);
+            }
         }
 
         private static void RealizarAção(OpçõesMenu opção)
@@ -56,20 +71,53 @@ namespace ColeçãoMagic
 
         private static void AdicionarCartas()
         {
-            throw new NotImplementedException();
+            string carta = LerCarta();
+            if (!Cartas.Contains(carta))
+            {
+                Cartas.Add(carta);
+                Console.WriteLine("Carta adicionada à sua coleção.");
+            }
+            else
+            {
+                Console.WriteLine("A sua coleção já possui esta carta.");
+            }
+        }
+
+        private static string LerCarta()
+        {
+            Console.WriteLine("Você optou por adicionar uma carta. Qual o nome da carta a ser adicionada:");
+            string cartaDigitada = Console.ReadLine();
+            return FormatarNomeCarta(cartaDigitada);
+        }
+
+        private static string FormatarNomeCarta(string cartaDigitada)
+        {
+            string[] formatandoCarta = cartaDigitada.Split(" ");
+            List<string> acumuladorString = new List<string>();
+            foreach (string palavra in formatandoCarta)
+            {
+                string nomeFormatado = palavra.Substring(0, 1).ToUpper() + palavra.Substring(1).ToLower();
+                acumuladorString.Add(nomeFormatado);
+            }
+            string cartaAdicionada = string.Join(" ", acumuladorString);
+            return cartaAdicionada;
         }
 
         private static void ImprimirColeção()
         {
-            throw new NotImplementedException();
+            bool checaConteúdoLista = Cartas.Any();
+            if (!checaConteúdoLista)
+            {
+                Console.WriteLine("Sua lista está vazia no momento.");
+            }
+            else
+            {
+                Console.WriteLine("Sua lista atual contém as seguintes cartas:");
+                foreach (string carta in Cartas)
+                {
+                    Console.WriteLine(carta);
+                }
+            }
         }
-    }
-
-    public enum OpçõesMenu
-    {
-        Sair = 0,
-        AdicionarCartas = 1,
-        ImprimirColeção = 2,
-        Inválida = 99
     }
 }
